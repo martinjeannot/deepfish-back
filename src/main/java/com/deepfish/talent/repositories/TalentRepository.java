c0b3c26c3fae642623ce35584372053d26d6e1e8
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-02-12 11:11:06 +0100

    Add querydsl support

--- src/main/java/com/deepfish/talent/repositories/TalentRepository.java
+++ src/main/java/com/deepfish/talent/repositories/TalentRepository.java
@@ -2,11 +2,13 @@ package com.deepfish.talent.repositories;
 
 import com.deepfish.talent.domain.Talent;
 import java.util.UUID;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface TalentRepository extends PagingAndSortingRepository<Talent, UUID> {
+public interface TalentRepository extends PagingAndSortingRepository<Talent, UUID>,
+    QueryDslPredicateExecutor<Talent> {
 
   Talent findByLinkedInId(String linkedInId);
 
