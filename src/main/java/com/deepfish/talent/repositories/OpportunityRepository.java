c579c4928fd9a699d7af42a69cdac9b112e253f8
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-02-21 17:20:59 +0100

    [Opportunity] enable querydsl on opportunity repository

--- src/main/java/com/deepfish/talent/repositories/OpportunityRepository.java
+++ src/main/java/com/deepfish/talent/repositories/OpportunityRepository.java
@@ -2,10 +2,12 @@ package com.deepfish.talent.repositories;
 
 import com.deepfish.talent.domain.opportunity.Opportunity;
 import java.util.UUID;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface OpportunityRepository extends PagingAndSortingRepository<Opportunity, UUID> {
+public interface OpportunityRepository extends PagingAndSortingRepository<Opportunity, UUID>,
+    QueryDslPredicateExecutor<Opportunity> {
 
 }
