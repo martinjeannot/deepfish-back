3235bcf9d9e794621fac77e53fc6f6c756ab408a
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-03-18 21:00:37 +0100

    [AdminDM Employer] fix search and remove querydsl from employer repository

--- src/main/java/com/deepfish/employer/domain/Employer.java
+++ src/main/java/com/deepfish/employer/domain/Employer.java
@@ -2,7 +2,6 @@ package com.deepfish.employer.domain;
 
 import com.deepfish.company.domain.Company;
 import com.deepfish.user.domain.AbstractUser;
-import com.querydsl.core.annotations.QueryEntity;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -11,7 +10,6 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Entity
-@QueryEntity
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Employer extends AbstractUser {
--- src/main/java/com/deepfish/employer/repositories/EmployerRepository.java
+++ src/main/java/com/deepfish/employer/repositories/EmployerRepository.java
@@ -1,26 +1,19 @@
 package com.deepfish.employer.repositories;
 
 import com.deepfish.employer.domain.Employer;
-import com.deepfish.employer.domain.QEmployer;
-import com.querydsl.core.types.dsl.StringExpression;
 import java.util.UUID;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
-import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
-import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface EmployerRepository extends PagingAndSortingRepository<Employer, UUID>,
-    QueryDslPredicateExecutor<Employer>,
-    QuerydslBinderCustomizer<QEmployer> {
+public interface EmployerRepository extends PagingAndSortingRepository<Employer, UUID> {
 
   Employer findByUsername(String username);
 
-  @Override
-  default void customize(QuerydslBindings bindings, QEmployer employer) {
-    bindings.bind(employer.username).first(StringExpression::contains);
-    bindings.bind(employer.firstName).first(StringExpression::contains);
-    bindings.bind(employer.lastName).first(StringExpression::contains);
-  }
+  Page<Employer> findByUsernameContainingOrFirstNameContainingOrLastNameContaining(
+      @Param("username") String username, @Param("firstName") String firstName,
+      @Param("lastName") String lastName, Pageable pageable);
 }
