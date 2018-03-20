6117c50662fdbe7b4f77558c8a1f0034741a5d39
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-03-20 16:48:02 +0100

    [AdminDM Employer] add fields to default employer projection

--- src/main/java/com/deepfish/employer/domain/DefaultEmployerProjection.java
+++ src/main/java/com/deepfish/employer/domain/DefaultEmployerProjection.java
@@ -1,6 +1,7 @@
 package com.deepfish.employer.domain;
 
 import com.deepfish.company.domain.Company;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.UUID;
 import org.springframework.data.rest.core.config.Projection;
@@ -11,7 +12,15 @@ public interface DefaultEmployerProjection {
 
   UUID getId();
 
+  String getUsername();
+
+  String getFirstName();
+
+  String getLastName();
+
   Collection<? extends GrantedAuthority> getAuthorities();
+
+  LocalDateTime getCreatedAt();
 
   Company getCompany();
 }
