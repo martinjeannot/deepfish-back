a8781097bc4d009125c05c5092bc5bcc3c1709a3
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-03-23 00:53:15 +0100

    [AdminDM] add talent search by mail or firstName or lastName

--- src/main/java/com/deepfish/talent/repositories/TalentRepository.java
+++ src/main/java/com/deepfish/talent/repositories/TalentRepository.java
@@ -2,8 +2,11 @@ package com.deepfish.talent.repositories;
 
 import com.deepfish.talent.domain.Talent;
 import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
@@ -13,4 +16,8 @@ public interface TalentRepository extends PagingAndSortingRepository<Talent, UUI
   Talent findByLinkedInIdOrEmail(String linkedInId, String email);
 
   Talent findByUsername(String username);
+
+  Page<Talent> findByEmailContainingOrLastNameContainingOrFirstNameContaining(
+      @Param("email") String email, @Param("lastName") String lastName,
+      @Param("firstName") String firstName, Pageable pageable);
 }
