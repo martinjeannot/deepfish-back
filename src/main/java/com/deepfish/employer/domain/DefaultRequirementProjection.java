c896c0ee6a7c8e59486bd88d56c6379362e33e0d
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-03-26 18:52:09 +0200

    [Requirement] add id to default projection

--- src/main/java/com/deepfish/employer/domain/DefaultRequirementProjection.java
+++ src/main/java/com/deepfish/employer/domain/DefaultRequirementProjection.java
@@ -4,10 +4,13 @@ import com.deepfish.company.domain.Company;
 import com.deepfish.talent.domain.conditions.Job;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.UUID;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "default", types = {Requirement.class})
 public interface DefaultRequirementProjection {
+
+  UUID getId();
 
   LocalDateTime getCreatedAt();
 
