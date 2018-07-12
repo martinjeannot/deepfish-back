2fcc855f946dda0832148789235472b1aa3762eb
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-07-12 11:34:26 +0200

    [DB migration] remove deprecated company maturity levels

---
+++ src/main/resources/db/migration/V2018.07.15__company_maturity_levels.sql
@@ -0,0 +1,10 @@
+-- Deprecated company maturity levels removal ======================================================
+
+DELETE FROM company_maturity_level
+WHERE id = '6ae9ce1a-cbef-41b7-bf3a-6ea6f85bcda9';
+
+DELETE FROM company_maturity_level
+WHERE id = '44df03f2-d552-4143-a7cb-5fc926a57542';
+
+DELETE FROM company_maturity_level
+WHERE id = 'a79199a8-3783-416f-a0fa-e9727627f8b2';
