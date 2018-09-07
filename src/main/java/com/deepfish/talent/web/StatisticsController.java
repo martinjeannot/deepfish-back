ff2c7aa57861c1b643d21ea451123e349fab0a36
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-09-07 12:46:33 +0200

    [Admin/Stats] add talent statistics API

--- src/main/java/com/deepfish/talent/web/StatisticsController.java
+++ src/main/java/com/deepfish/talent/web/StatisticsController.java
@@ -17,6 +17,46 @@ public class StatisticsController {
     this.entityManager = entityManager;
   }
 
+  @GetMapping("talents/statistics")
+  @ResponseBody
+  public ResponseEntity getTalentAcquisitionStatistics(
+      @RequestParam("created-at-after") String createdAtAfter,
+      @RequestParam("created-at-before") String createdAtBefore,
+      @RequestParam("group-by") String groupBy) {
+    String datePattern;
+    String interval;
+    switch (groupBy) {
+      case "day":
+        datePattern = "YYYY-MM-DD";
+        interval = "1 day";
+        break;
+      case "week":
+        datePattern = "YYYY-WW";
+        interval = "1 week";
+        break;
+      case "month":
+        datePattern = "YYYY-MM";
+        interval = "1 month";
+        break;
+      case "year":
+        datePattern = "YYYY";
+        interval = "1 year";
+        break;
+      default:
+        throw new IllegalArgumentException("Unknown group-by value : " + groupBy);
+    }
+    Query query = entityManager.createNativeQuery(
+        "SELECT dtime, coalesce(talents, 0) AS talents "
+            + "FROM (SELECT to_char(time_series, :date_pattern) AS dtime FROM generate_series(cast(:start_date AS DATE), cast(:end_date AS DATE), cast(:time_interval AS INTERVAL)) time_series) all_times "
+            + "LEFT JOIN (SELECT to_char(created_at, :date_pattern) AS dtime, count(1) AS talents FROM Talent GROUP BY dtime) talents "
+            + "USING (dtime) ORDER BY dtime");
+    query.setParameter("date_pattern", datePattern);
+    query.setParameter("start_date", createdAtAfter);
+    query.setParameter("end_date", createdAtBefore);
+    query.setParameter("time_interval", interval);
+    return ResponseEntity.ok(query.getResultList());
+  }
+
   @GetMapping("opportunities/statistics")
   @ResponseBody
   public ResponseEntity getOpportunityStatistics(@RequestParam("datepart") String datepart) {
