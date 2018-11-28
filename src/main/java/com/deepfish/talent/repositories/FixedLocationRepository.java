cea9cdb766ec924683b608dff1b9ff4da3e92ffa
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-11-28 16:47:32 +0100

    [Talent/Conditions] Add QueryDSL support for fixed locations endpoint

--- src/main/java/com/deepfish/talent/repositories/FixedLocationRepository.java
+++ src/main/java/com/deepfish/talent/repositories/FixedLocationRepository.java
@@ -2,10 +2,12 @@ package com.deepfish.talent.repositories;
 
 import com.deepfish.talent.domain.conditions.FixedLocation;
 import java.util.UUID;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface FixedLocationRepository extends PagingAndSortingRepository<FixedLocation, UUID> {
+public interface FixedLocationRepository extends PagingAndSortingRepository<FixedLocation, UUID>,
+    QueryDslPredicateExecutor<FixedLocation> {
 
 }
