e1f4828dee69ad9c6b01412c74e4e5b0ef14e0f8
Martin Jeannot <m4rtinjeannot@gmail.com>
2018-12-09 20:02:07 +0100

    [Opportunity] add expiration date to talent projection

--- src/main/java/com/deepfish/talent/domain/opportunity/TalentOpportunityProjection.java
+++ src/main/java/com/deepfish/talent/domain/opportunity/TalentOpportunityProjection.java
@@ -3,6 +3,7 @@ package com.deepfish.talent.domain.opportunity;
 import com.deepfish.company.domain.DefaultCompanyProjection;
 import com.deepfish.employer.domain.requirement.Requirement;
 import com.deepfish.talent.domain.conditions.JobType;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
@@ -11,6 +12,9 @@ import org.springframework.data.rest.core.config.Projection;
 public interface TalentOpportunityProjection {
 
   UUID getId();
+
+  @Value("#{target.getCreatedAt().plusDays(T(com.deepfish.talent.domain.opportunity.Opportunity).LIFESPAN_DAYS)}")
+  LocalDateTime getExpiredAt();
 
   OpportunityStatus getTalentStatus();
 
