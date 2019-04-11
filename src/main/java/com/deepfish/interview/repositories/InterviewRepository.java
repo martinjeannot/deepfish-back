1b54539fa7dca7ac66ed523c1a29277733db913c
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-04-11 17:22:31 +0200

    [Itw] add findByOpportunityIdAndCurrentlyTalentPendingOrConfirmed endpoint

--- src/main/java/com/deepfish/interview/repositories/InterviewRepository.java
+++ src/main/java/com/deepfish/interview/repositories/InterviewRepository.java
@@ -1,15 +1,35 @@
 package com.deepfish.interview.repositories;
 
 import com.deepfish.interview.domain.Interview;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
 public interface InterviewRepository extends PagingAndSortingRepository<Interview, UUID>,
     QueryDslPredicateExecutor<Interview> {
 
   List<Interview> findBySharedId(UUID sharedId);
+
+  @PreAuthorize("hasRole('TALENT')")
+  @PostAuthorize("returnObject.?[talent.linkedinId != #root.principal].size() == 0")
+  @Query("select itw from #{#entityName} itw "
+      + "join itw.opportunity opp on opp.id = :opportunityId "
+      + "where itw.startDateTime > :startAfter "
+      + "and itw.talentResponseStatus = '#{T(com.deepfish.interview.domain.ParticipationStatus).NEEDS_ACTION}' "
+      + "or itw.status = '#{T(com.deepfish.interview.domain.InterviewStatus).CONFIRMED}' "
+      + "order by itw.startDateTime desc")
+  List<Interview> findByOpportunityIdAndCurrentlyTalentPendingOrConfirmed(
+      @Param("opportunityId") UUID opportunityId,
+      @Param("startAfter") @DateTimeFormat(iso = ISO.DATE_TIME) ZonedDateTime startAfter
+  );
 }
