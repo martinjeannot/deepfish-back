5f92a224a446d951f799e3a2ea23d54784b123e9
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-06-24 00:34:16 +0200

    [Talent/Opportunity] add find interview-schedulable opportunities by company name repository method

--- src/main/java/com/deepfish/talent/repositories/OpportunityRepository.java
+++ src/main/java/com/deepfish/talent/repositories/OpportunityRepository.java
@@ -6,10 +6,12 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 @RepositoryRestResource
 public interface OpportunityRepository extends PagingAndSortingRepository<Opportunity, UUID>,
@@ -24,6 +26,19 @@ public interface OpportunityRepository extends PagingAndSortingRepository<Opport
   Page<Opportunity> findByRequirementId(
       @Param("requirementId") UUID requirementId,
       Pageable pageable);
+
+  @PreAuthorize("hasRole('ADMIN')")
+  @Query("select opp from #{#entityName} opp "
+      + "join opp.requirement req "
+      + "join req.company comp "
+      + "where opp.talent.id = :talentId "
+      + "and opp.employerStatus in ('#{T(com.deepfish.talent.domain.opportunity.OpportunityStatus).PENDING}', '#{T(com.deepfish.talent.domain.opportunity.OpportunityStatus).ACCEPTED}') "
+      + "and upper(comp.name) like upper(concat('%', :companyName, '%'))")
+  Page<Opportunity> findInterviewSchedulableByTalentIdAndCompanyNameContainingAllIgnoreCase(
+      @Param("talentId") UUID talentId,
+      @Param("companyName") String companyName,
+      Pageable pageable
+  );
 
   Page<Opportunity> findByTalentStatusAndCreatedAtBetween(
       OpportunityStatus talentStatus,
