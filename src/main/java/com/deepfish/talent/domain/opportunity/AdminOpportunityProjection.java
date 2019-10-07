850c874678def6538697401b44564ce34f104ef1
Martin Jeannot <m4rtinjeannot@gmail.com>
2019-10-07 19:08:53 +0200

    [Talent/Opportunity] add employer accepted/declined at fields to admin projection

--- src/main/java/com/deepfish/talent/domain/opportunity/AdminOpportunityProjection.java
+++ src/main/java/com/deepfish/talent/domain/opportunity/AdminOpportunityProjection.java
@@ -21,7 +21,9 @@ public interface AdminOpportunityProjection extends AdminItemOpportunityProjecti
 
   LocalDateTime getForwardedAt();
 
-  LocalDateTime getEmployerRespondedAt();
+  LocalDateTime getEmployerAcceptedAt();
+
+  LocalDateTime getEmployerDeclinedAt();
 
   LocalDate getTalentStartedOn();
 
