105e0ba7ea412264ce4235c34a1bd729ebf4ce83
Martin Jeannot <m4rtinjeannot@gmail.com>
2020-01-10 11:48:22 +0100

    [Employer] add query DSL to employer repository

--- src/main/java/com/deepfish/employer/repositories/EmployerRepository.java
+++ src/main/java/com/deepfish/employer/repositories/EmployerRepository.java
@@ -4,12 +4,14 @@ import com.deepfish.employer.domain.Employer;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface EmployerRepository extends PagingAndSortingRepository<Employer, UUID> {
+public interface EmployerRepository extends PagingAndSortingRepository<Employer, UUID>,
+    QueryDslPredicateExecutor<Employer> {
 
   Employer findByUsername(String username);
 
